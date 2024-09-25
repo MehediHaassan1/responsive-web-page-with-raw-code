6dc3244e5250d1d1a6de97f2c046c3b07ac2021d
Mehedi Hassan Barat <mehedi.haassan1@gmail.com>
2024-09-25 12:46:20 +0600

    responsive modal done

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 let nav = document.querySelector('nav');
 let menuList = document.getElementById("menuList");
 let menuIcon = document.getElementById("menuIcon").firstElementChild;
+let modal = document.getElementById("modal");
+let closeButton = document.querySelector(".close");
 
 
 menuList.style.maxHeight = "0px";
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    menuList.style.maxHeight = "0px";
+    menuIcon.classList.remove("fa-xmark");
+    menuIcon.classList.add("fa-bars");
+    nav.classList.add('blur');
+  } else {
+    nav.classList.remove('blur');
+  }
+});
+
 
 function toggleMenu() {
   if (menuList.style.maxHeight == "0px") {
@@ -16,3 +30,32 @@ function toggleMenu() {
     menuIcon.classList.add("fa-bars");
   }
 }
+
+
+
+document.querySelectorAll('.card').forEach(card => {
+  card.addEventListener('click', function () {
+    const image = this.dataset.image;
+    const title = this.dataset.title;
+    const description = this.dataset.description;
+
+    document.getElementById('modal-image').src = image;
+    document.getElementById('modal-title').textContent = title;
+    document.getElementById('modal-description').textContent = description;
+
+    modal.style.display = "block";
+    document.body.classList.add('no-scroll');
+  });
+});
+
+closeButton.onclick = function () {
+  modal.style.display = "none";
+  document.body.classList.remove('no-scroll');
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    document.body.classList.remove('no-scroll');
+  }
+};

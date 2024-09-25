b7674939fed3991cc115ed62863db351c7e5eea7
Mehedi Hassan Barat <mehedi.haassan1@gmail.com>
2024-09-25 10:32:09 +0600

    responsive navbar done

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('Js file loaded.')
+let menuList = document.getElementById("menuList");
+let nav = document.querySelector('nav');
+let menuIcon = document.getElementById("menuIcon").firstElementChild;
+
+
+menuList.style.maxHeight = "0px";
+
+function toggleMenu() {
+  if (menuList.style.maxHeight == "0px") {
+    menuList.style.maxHeight = "100vh";
+    menuIcon.classList.remove("fa-bars");
+    menuIcon.classList.add("fa-xmark");
+  } else {
+    menuList.style.maxHeight = "0px";
+    menuIcon.classList.remove("fa-xmark");
+    menuIcon.classList.add("fa-bars");
+  }
+}
